6b4e99a17a2809344da95041bb893e64e3281bbb
carpenter-js <carpenterbarrett@gmail.com>
2019-04-02 15:23:23 -0600

    added referral view

--- VenvDjango/code-challenge/project/referrals/views.py
+++ VenvDjango/code-challenge/project/referrals/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from referrals.models import Referral
+from referrals.serializers import ReferralSerializer
+from rest_framework import generics
 
-# Create your views here.
+class ReferralListCreate(generics.ListCreateAPIView):
+  queryset = Referral.objects.all()
+  serializer_class = ReferralSerializer
