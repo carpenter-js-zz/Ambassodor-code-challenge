951f47fa86d9b6eb747be099e45acc961a8222e3
carpenter-js <carpenterbarrett@gmail.com>
2019-04-02 15:42:49 -0600

    set up referral url, added referrals fixture

--- VenvDjango/code-challenge/project/referrals/models.py
+++ VenvDjango/code-challenge/project/referrals/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Referral(models.Model):
   title = models.CharField(max_length=100)
-  click_count = models.IntegerField()
+  click_count = models.IntegerField(default=0)
   created_at = models.DateTimeField(auto_now_add=True)
