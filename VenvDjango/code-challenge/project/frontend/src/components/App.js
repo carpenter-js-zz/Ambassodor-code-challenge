0fe6f2f8a2460ed91e6a462062a1c5055d9d93b5
carpenter-js <carpenterbarrett@gmail.com>
2019-04-06 14:48:20 -0600

    debugging re_path

--- VenvDjango/code-challenge/project/frontend/src/components/App.js
+++ VenvDjango/code-challenge/project/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import MainPage from './MainPage';
 import LandingPage from './LandingPage';
